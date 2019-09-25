bbee6d901e39b48cb15659495e8c2b86476abd8f
emilekm2142 <emilm7843@gmail.com>
2019-09-25 21:08:40 +0200

    initial

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import send_file
+import json
+app = Flask(__name__)
+CURRENT_VERSION = 2
+newest_version_dir = 'newest/newest.bin'
+@app.route('/update/<version>')
+def update(version:int):
+    if (int(version) < CURRENT_VERSION):
+        return '1'
+    else:
+        return '0'
+
+@app.route("/update/get")
+def get_update():
+    return send_file("newest.bin")
+
+@app.route("/")
+def index():
+    return "Changelog:"
